415c1cd10bd2fa02394abad9132ed71491b41f10
mahmudulhsn <hasib.mahmudul@gmail.com>
2020-11-26 18:14:22 +0600

    till relation between user and post

---
+++ src/index.js
@@ -0,0 +1,138 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+const users = [
+    {
+        id: '1',
+        name: 'Hasib',
+        email: 'hasib@example.com',
+        age: 60
+    },
+    {
+        id: '2',
+        name: 'Demo',
+        email: 'demo@example.com',
+        age: 60
+    },
+    {
+        id: '3',
+        name: 'Demo 2',
+        email: 'demo2@example.com',
+        age: 60
+    },
+    {
+        id: '4',
+        name: 'Demo 3',
+        email: 'demo3@example.com',
+        age: 60
+    }
+]
+
+const posts = [
+    {
+        id: '1',
+        title: 'What is Lorem Ipsum?',
+        body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        published: true,
+        author: '1'
+    },
+    {
+        id: '2',
+        title: 'Why do we use it?',
+        body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+        published: true,
+        author: '2'
+    },
+    {
+        id: '3',
+        title: 'Where does it come from?',
+        body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+        published: false,
+        author: '2'
+    },
+]
+
+const typeDefs = `
+    type Query {
+        users(query: String): [User!]!
+        posts(query: String) : [Post!]!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+    }
+    
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+    }
+`
+const resolvers = {
+    Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+                return user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+            })
+        },
+        me() {
+            return {
+                id: 'abc123',
+                name: 'M H Hasib',
+                email: 'hasib@hasib.com',
+                age: null
+            }
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+        
+            return posts.filter((post) => {
+                return post.title.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+            })
+        },        
+        post() {
+            return {
+                id: 'adcd123',
+                title: 'This is a Post',
+                body: 'This is body',
+                published: true
+            }
+        }
+    },
+    Post: {
+        author (parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id == parent.author
+            })
+        }
+    },
+    User: {
+        posts (parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author == parent.id
+            })
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('Server Starts!')
+})
